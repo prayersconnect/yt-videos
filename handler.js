8f202666c56ee8a8893193a810e68570c8d8abc6
The HungryCoder <rajuru@gmail.com>
2020-10-21 22:53:15 -0500

    use timeout method to wait for multi page load finished!

--- handler.js
+++ handler.js
@@ -3,19 +3,28 @@ const scraper = require('./scrap');
 
 module.exports.getChannelVideos = async event => {
   try {
-    // const channelOrPlaylistId = 'UCCMC_4hcI9zoOvjSfka0-xQ';
-    const channelOrPlaylistId = (event.queryStringParameters || {}).id;
-    console.log(channelOrPlaylistId);
-    const videos = await scraper.getChannelVideos(channelOrPlaylistId);
+    console.log(`Function version: ${process.env.AWS_LAMBDA_FUNCTION_VERSION}`);
+    const channelOrUser = (event.queryStringParameters || {}).id;
+    const maxPages = (event.queryStringParameters || {}).max_pages;
+    const kind = (event.queryStringParameters || {}).type;
+
+    console.log('params', channelOrUser, maxPages, kind);
+
+    let videos;
+    if (kind === 'user') {
+      videos = await scraper.getUserVideos(channelOrUser, maxPages);
+    } else {
+      videos = await scraper.getChannelVideos(channelOrUser, maxPages);
+    }
 
     return {
       statusCode: 200,
       body: JSON.stringify(videos)
     };
-  } catch(err) {
+  } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify([err.message])
-    }   
+    }
   }
 };
