241d5aae7483f3f1fdbe46a62d7dd8bd825bdeed
Raju <rajuru@gmail.com>
2021-02-05 22:01:57 -0600

    simplify scrap codes

--- handler.js
+++ handler.js
@@ -1,26 +1,17 @@
 'use strict';
 const scraper = require('./scrap');
-const ytch = require('yt-channel-info');
 
 module.exports.getVideoList = async event => {
   try {
     console.log(`Function version: ${process.env.AWS_LAMBDA_FUNCTION_VERSION}`);
     const channelOrUser = (event.queryStringParameters || {}).id;
     const maxPages = (event.queryStringParameters || {}).max_pages;
-    const kind = (event.queryStringParameters || {}).type;
 
-    console.log('params', channelOrUser, maxPages, kind);
-
-    let videos;
-    if (kind === 'user') {
-      videos = await scraper.getUserVideos(channelOrUser, maxPages);
-    } else {
-      videos = await scraper.getChannelVideos(channelOrUser, maxPages);
-    }
+    const videos = await scraper.getVideos(channelOrUser, maxPages);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(videos)
+      body: videos
     };
   } catch (err) {
     return {
@@ -30,21 +21,3 @@ module.exports.getVideoList = async event => {
   }
 };
 
-
-module.exports.getVideoDetails = async event => {
-  try {
-    console.log(`Function version: ${process.env.AWS_LAMBDA_FUNCTION_VERSION}`);
-    const videoId = (event.queryStringParameters || {}).id;
-    console.log('videoId', videoId);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(await scraper.getVideoDetails([videoId]))
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify([err.message])
-    }
-  }
-};
--- scrap.js
+++ scrap.js
@@ -1,45 +1,29 @@
 const ytch = require('yt-channel-info');
-const { Youtube } = require('scrape-youtube');
 
+function parseVideo(details) {
 
-const getChannelVideos = async (channelId, maxPage) => {
-  const videos = await ytch.getChannelVideos(channelId, 'newest');
-  return videos.items.map((vid)=>vid.videoId);
+  const thumbnail = details.videoThumbnails.sort((vid1, vid2) => vid2.width - vid1.width)[0];
+
+  return {
+    title: details.title,
+    description: null,
+    duration: details.lengthSeconds,
+    thumbnail: thumbnail.url,
+    link: `https://www.youtube.com/watch?v=${details.videoId}`
+  }
 }
 
-const getUserVideos = async (userId, maxPage) => {
-  const url = `https://m.youtube.com/c/${userId}/videos`;
-  const videos = parseVideos(await getURLContents(url, maxPage));
-
-  console.log('video list', videos, `Total ${videos.length} videos`);
-
-  return videos;
-}
-
-const getVideoDetails = async(videos = []) => {
-  const youtube = new Youtube();
-  return await Promise.all(videos.map(async (videoId) => {
-    const url = `https://www.youtube.com/watch?v=${videoId}`
-    const videoDetail =  await youtube.searchOne(url)
-    // console.log('video detail:', videoDetail)
-    return videoDetail;
-  }));
+const getVideos = async (channelOrUserId, maxPage) => {
+  const videos = await ytch.getChannelVideos(channelOrUserId, 'newest');
+  return videos.items.filter((vid) => vid.authorId === channelOrUserId).map(parseVideo);
 }
 
 if (require.main === module) {
-  (async() => {
-      // getUserVideos('ValleyRanchIslamicCenter', 5);
-    console.log(await getChannelVideos('UCCMC_4hcI9zoOvjSfka0-xQ', 1));
-
-    // const details = await getVideoDetails(['qSP_BP-pM4w', 'ra-5KkKrbXg', '-7DHxq4PBoQ', 'zZgcOpJOAWo']);
-    // console.log('details', details);
+  (async () => {
+    console.log(await getVideos('UCCMC_4hcI9zoOvjSfka0-xQ', 1));
   })()
-
-
 }
 
 module.exports = {
-  getChannelVideos,
-  getUserVideos,
-  getVideoDetails
+  getVideos
 }
